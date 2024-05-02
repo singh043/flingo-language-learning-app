593b99f2967896bcbd0c1c94b424884e846c9de3
singh043 <kumarjatin431109@gmail.com>
2024-05-02 17:00:10 +0530

    learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,19 @@
-const Learn = () => {
+import { Header } from "./header";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
+const LearnPage = () => {
     return (
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap[48px] px-6">
+            <StickyWrapper>
+                <UserProgress activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }} hearts={5} points={100} hasActiveSubscription={false} />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish" />
+            </FeedWrapper>
         </div>
     )
 }
 
-export default Learn;
+export default LearnPage;
